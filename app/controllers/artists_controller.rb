8d2b1f3a9c81688ee9c96e89462ce38924f75fe1
renato niola <niolarenato01@gmail.com>
2018-01-20 16:09:50 +0100

    create controller and index,show views for artist with basic functionality

---
+++ app/controllers/artists_controller.rb
@@ -0,0 +1,17 @@
+class ArtistsController < ApplicationController
+  before_action :set_artist, only: [:show]
+
+  def index
+    @artists = Artist.all
+  end
+
+  def show
+  end
+
+  private
+
+  def set_artist
+    @artist= Artist.find(params[:id])
+  end
+
+end
